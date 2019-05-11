6e5887caad1e11716ffbb677771eff4d5907cbce
Mohammad Hosein Etefaghian <etefaghian@yahoo.com>
2019-05-11 10:28:35 +0430

    init commit

---
+++ src/test/java/ir/etefaghian/hotelmanagment/HotelManagmentApplicationTests.java
@@ -0,0 +1,16 @@
+package ir.etefaghian.hotelmanagment;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class HotelManagmentApplicationTests {
+
+    @Test
+    public void contextLoads() {
+    }
+
+}
